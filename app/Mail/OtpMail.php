055962eeaffc313f10087f4e6ca032c9980c70d9
denzelboye <denzelthmone21@gmail.com>
2026-04-14 09:58:54 +0000

    Fix OtpMail class not found - correct casing and autoload

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class OTPMail extends Mailable
+class OtpMail extends Mailable
 {
     use Queueable, SerializesModels;
 
